46e0c202c72a50b367cbf7e294afaba461505745
dominicrann61 <dominic568@hotmail.com>
2019-05-25 17:35:30 +0800

    added comments
    added helpfull comments

--- chrome_extensions/01_content_scripts/content.js
+++ chrome_extensions/01_content_scripts/content.js
@@ -1,11 +1,13 @@
+
+// we will see this log in the browser js console
 console.log('Content Script Running');
 
 
 
-
+// get all the paragraphs
  let p = document.getElementsByTagName('p');
-
+    //loop through all those paragraphs
         for (p of p) {
-
+        //set all the paragraphs background-color to purple
             p.style['background-color'] = '#A020F0';
         }
